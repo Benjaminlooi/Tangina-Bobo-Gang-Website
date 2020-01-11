03d8b2a4674bea32c5a5b53f37d7ced278e58957
Benjamin Looi <benjaminlooidt@gmail.com>
2020-01-11 20:53:01 +0800

    Added hero image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="home">
     <div class="hero">
-      <div>
+      <div class="hero-text-wrapper">
         <p class="text-xs uppercase">Diary</p>
         <h1 class="text-4xl font-bold uppercase">Tanginabobo Gang</h1>
+      </div>
+      <div class="hero-img-wrapper">
+        <img src="../assets/hero.jpeg" alt />
       </div>
     </div>
   </div>
@@ -22,9 +25,25 @@ export default {
   height: 95vh;
   margin: auto;
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
   padding: 0 50px;
   box-sizing: border-box;
   letter-spacing: 0.12em;
+}
+.hero-text-wrapper {
+  width: 50%;
+  z-index: 1;
+}
+.hero-img-wrapper {
+  width: 50%;
+}
+@media only screen and (max-width: 690px) {
+  .hero-text-wrapper {
+    width: 100%;
+  }
+  .hero-img-wrapper {
+    width: 100%;
+  }
 }
 </style>
